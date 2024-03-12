0a40ac22cc616c65a8b875f9b4f28eb7157b6bd8
fharlup <35958282+fharlup@users.noreply.github.com>
2024-03-12 13:29:11 +0700

    kedua

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Fungsi
+{
+    enum Kelurahan
+    {
+        Batununggal, Kujangsari, Mengger, Wates,
+        Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja
+    }
+
+    enum Kode
+    {
+        Kode1 = 40266,
+        Kode2 = 40287,
+        Kode3 = 40287,
+        Kode4 = 40256,
+        Kode5 = 40287,
+        Kode6 = 40287,
+        Kode7 = 40286,
+        Kode8 = 40286,
+        Kode9 = 40272,
+        Kode10 = 40274,
+        Kode11 = 40273
+    };
+
+    static void Main(string[] args)
+    {
+       foreach (Kelurahan kelurahan in Enum.GetValues(typeof(Kelurahan)))
+        {
+            int pos = (int)Enum.Parse(typeof(Kode), "Kode" + ((int)kelurahan + 1));
+            Console.WriteLine($"kelurahan:{kelurahan}kode:{pos}");
+        }
+    }
+}
