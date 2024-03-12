6a8fe1fdfb2a14b356d93075d9e8819042768a3f
fharlup <35958282+fharlup@users.noreply.github.com>
2024-03-12 16:18:45 +0700

    commit ke 2

---
+++ lock/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class DoorMachine
+{
+    static void Main(string[] args)
+    {
+        Door door = new Door();
+        door.Close();
+        door.Lock();
+        door.Unlock();
+        door.Open();
+    }
+}
+
+class Door
+{
+    private bool isOpen;
+    private bool isLocked;
+
+    public Door()
+    {
+        isOpen = false;
+        isLocked = false;
+    }
+
+    public void Open()
+    {
+        if (!isLocked)
+        {
+            isOpen = true;
+            Console.WriteLine("The door is now open.");
+        }
+        else
+        {
+            Console.WriteLine("The door is locked. Unlock it first.");
+        }
+    }
+
+    public void Close()
+    {
+        isOpen = false;
+        Console.WriteLine("The door is now closed.");
+    }
+
+    public void Lock()
+    {
+        isLocked = true;
+        Console.WriteLine("The door is now locked.");
+    }
+
+    public void Unlock()
+    {
+        isLocked = false;
+        Console.WriteLine("The door is now unlocked.");
+    }
+}
---
+++ lock/ConsoleApp2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class DoorMachine
+{
+    public enum StatePintu
+    {
+        Terkunci, Terbuka
+    };
+
+    public enum Triger
+    {
+        KunciPintu, BukaPintu
+    }
+
+    public class Transition
+    {
+        public StatePintu stateAwal;
+        public StatePintu stateAkhir;
+        public Triger triger;
+
+        public Transition(StatePintu stateAwal, StatePintu stateAkhir, Triger triger)
+        {
+            this.stateAwal = stateAwal;
+            this.stateAkhir = stateAkhir;
+            this.triger = triger;
+        }
+    }
+
+    Transition[] transitions = {
+        new Transition(StatePintu.Terkunci, StatePintu.Terbuka, Triger.BukaPintu),
+        new Transition(StatePintu.Terbuka, StatePintu.Terkunci, Triger.KunciPintu)
+    };
+
+    public StatePintu curentState = StatePintu.Terkunci;
+
+    public StatePintu GetNextState(StatePintu stateAwal, Triger triger)
+    {
+        StatePintu stateAkhir = stateAwal;
+        for (int i = 0; i < transitions.Length; i++)
+        {
+            Transition perubahan = transitions[i];
+            if (stateAwal == perubahan.stateAwal && triger == perubahan.triger)
+            {
+                stateAkhir = perubahan.stateAkhir;
+            }
+        }
+        return stateAkhir;
+    }
+
+    public void ActivateTriger(Triger triger)
+    {
+        curentState = GetNextState(curentState, triger);
+        Console.WriteLine("pintu sekarang " + curentState);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DoorMachine pintu = new DoorMachine();
+        Console.WriteLine("pintu sekarang " + pintu.curentState);
+        pintu.ActivateTriger(DoorMachine.Triger.BukaPintu);
+        pintu.ActivateTriger(DoorMachine.Triger.KunciPintu); 
+    }
+}
